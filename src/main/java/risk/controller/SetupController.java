20088ef32d369cdba7eacff106d38af6c55065a3
Nicholas W. Hall <hallnw@hallnw-stg4.rose-hulman.edu>
2022-04-01 16:35:57 -0400

    Completed Inappropriate intimacy, Data clump, and Feature envy code smells

--- src/main/java/risk/controller/SetupController.java
+++ src/main/java/risk/controller/SetupController.java
@@ -3,10 +3,8 @@ package risk.controller;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import risk.model.PlayerModel;
-import risk.model.SixSidedDie;
-import risk.model.StaticResourceBundle;
-import risk.model.TerritoryModel;
+
+import risk.model.*;
 import risk.view.GameView;
 
 public class SetupController implements Runnable, GameViewObserver {
@@ -107,8 +105,9 @@ public class SetupController implements Runnable, GameViewObserver {
 	private void placeArmies(){
 		for(int i = 0; i < playerModels.size(); i++){
 			currentPlayer = i;
-			for(int j = 0; j < playerModels.get(i).getCardCount(); j++){
-				claimTerritory(playerModels.get(i).getCardAtIndex(j).getTerritoryName());
+			CardManager cards = playerModels.get(i).getCards();
+			for(Card card : cards.getTotalCards()){
+				claimTerritory(card.getTerritoryName());
 			}
 		}
 		currentPlayer = 0;
